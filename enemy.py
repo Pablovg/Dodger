ed2149418b865982803595afa0d501a8e5342f49
Pablovg <pablovgdev@gmail.com>
2017-09-11 17:07:51 +0200

    optimized

--- enemy.py
+++ enemy.py
@@ -1,53 +1,57 @@
 # IMPORTS
 import pygame as pg
-from random import randint
-from colors import *
+import colors as c
+
 
 class enemy:
-	
-	def __init__(self, surface, BS, BSI):
-		self.surface = surface
-		self.bs = BS
-		self.bsi = BSI
-		
-		# for drawing the block
-		self.enemyblock = pg.Surface((BS, BS))
-		self.enemyblock.set_alpha(255)
-		self.enemyblock.fill(RED)
-		self.enemyblockdark = pg.Surface((BSI, BSI))
-		self.enemyblockdark.set_alpha(255)
-		self.enemyblockdark.fill(RED_DARK)
-		
-		# for deleting blocks
-		self.blackblock = pg.Surface((BS, BS))
-		self.blackblock.set_alpha(255)
-		self.blackblock.fill(BLACK)
 
-	def setPos(self, posx, posy):
-		self.pos = [posx, posy]
-		
-	def getPos(self):
-		return self.pos
-		
-	def isEnemy():
-		return True
+    def __init__(self, surface, BS, BSI):
+        self.surface = surface
+        self.bs = BS
+        self.bsi = BSI
 
-	def move(self, move):
-		pos = self.getPos()
-		self.surface.blit(self.blackblock, (pos[0]*self.bs, pos[1]*self.bs))
+        # for drawing the block
+        self.enemyblock = pg.Surface((BS, BS))
+        self.enemyblock.set_alpha(255)
+        self.enemyblock.fill(c.RED)
+        self.enemyblockdark = pg.Surface((BSI, BSI))
+        self.enemyblockdark.set_alpha(255)
+        self.enemyblockdark.fill(c.RED_DARK)
 
-		# update positions
-		if move == 1: #UP
-			pos = [pos[0], pos[1]-1]
-		elif move == 2: #DOWN
-			pos = [pos[0], pos[1]+1]
-		elif move == 3: #LEFT
-			pos = [pos[0]-1, pos[1]]
-		elif move == 4: #RIGHT
-			pos = [pos[0]+1, pos[1]]
+        # for deleting blocks
+        self.blackblock = pg.Surface((BS, BS))
+        self.blackblock.set_alpha(255)
+        self.blackblock.fill(c.BLACK)
 
-		self.pos = pos
+    def setPos(self, posx, posy):
+        self.pos = [posx, posy]
 
-	def draw(self):
-		self.surface.blit(self.enemyblock, (self.pos[0]*self.bs, self.pos[1]*self.bs))
-		self.surface.blit(self.enemyblockdark,(self.pos[0]*self.bs+1, self.pos[1]*self.bs+1))
+    def getPos(self):
+        return self.pos
+
+    def isEnemy():
+        return True
+
+    def move(self, move):
+        pos = self.getPos()
+        self.surface.blit(self.blackblock,
+                          (pos[0] * self.bs, pos[1] * self.bs))
+
+        # update positions
+        if move == 1:  # UP
+            pos = [pos[0], pos[1] - 1]
+        elif move == 2:  # DOWN
+            pos = [pos[0], pos[1] + 1]
+        elif move == 3:  # LEFT
+            pos = [pos[0] - 1, pos[1]]
+        elif move == 4:  # RIGHT
+            pos = [pos[0] + 1, pos[1]]
+
+        self.pos = pos
+
+    def draw(self):
+        self.surface.blit(self.enemyblock,
+                          (self.pos[0] * self.bs, self.pos[1] * self.bs))
+        self.surface.blit(self.enemyblockdark,
+                          (self.pos[0] * self.bs + 1,
+                           self.pos[1] * self.bs + 1))
--- player.py
+++ player.py
@@ -1,62 +1,65 @@
 # IMPORTS
 import pygame as pg
-from colors import *
+import colors as c
 
 # DIRECTIONS
 NONE = 0
-UP    = 1
-DOWN  = 2
-LEFT  = 3
+UP = 1
+DOWN = 2
+LEFT = 3
 RIGHT = 4
+
 
 class player:
 
-	# constructor
-	def __init__(self, surface, POSX, POSY, BS, BSI):
-		self.surface = surface
-		self.pos = [POSX, POSY]
-		self.bs = BS
-		self.bsi = BSI
-		self.direction = NONE
+    # constructor
+    def __init__(self, surface, POSX, POSY, BS, BSI):
+        self.surface = surface
+        self.pos = [POSX, POSY]
+        self.bs = BS
+        self.bsi = BSI
+        self.direction = NONE
 
-		# for drawing the block
-		self.playerblock = pg.Surface((BS, BS))
-		self.playerblock.set_alpha(255)
-		self.playerblock.fill(YELLOW)
-		self.playerblockdark = pg.Surface((BSI,BSI))
-		self.playerblockdark.set_alpha(255)
-		self.playerblockdark.fill(YELLOW_DARK)
-		
-		# for deleting blocks
-		self.blackblock = pg.Surface((BS, BS))
-		self.blackblock.set_alpha(255)
-		self.blackblock.fill(BLACK)
+        # for drawing the block
+        self.playerblock = pg.Surface((BS, BS))
+        self.playerblock.set_alpha(255)
+        self.playerblock.fill(c.YELLOW)
+        self.playerblockdark = pg.Surface((BSI, BSI))
+        self.playerblockdark.set_alpha(255)
+        self.playerblockdark.fill(c.YELLOW_DARK)
 
+        # for deleting blocks
+        self.blackblock = pg.Surface((BS, BS))
+        self.blackblock.set_alpha(255)
+        self.blackblock.fill(c.BLACK)
 
-	def getPos(self):
-		return self.pos
-		
-	def getDirection(self):
-		return self.direction
-	
-	def move(self, move):
-		pos = self.getPos()
-		self.surface.blit(self.blackblock,(pos[0]*self.bs, pos[1]*self.bs))
-		self.direction = move
+    def getPos(self):
+        return self.pos
 
-		# update positions
-		if move == 1: #UP
-			pos = [pos[0], pos[1]-1]
-		elif move == 2: #DOWN
-			pos = [pos[0], pos[1]+1]
-		elif move == 3: #LEFT
-			pos = [pos[0]-1, pos[1]]
-		elif move == 4: #RIGHT
-			pos = [pos[0]+1, pos[1]]
+    def getDirection(self):
+        return self.direction
 
-		self.pos = pos
+    def move(self, move):
+        pos = self.getPos()
+        self.surface.blit(self.blackblock,
+                          (pos[0] * self.bs, pos[1] * self.bs))
+        self.direction = move
 
-	def draw(self):
-		self.surface.blit(self.playerblock, (self.pos[0]*self.bs, self.pos[1]*self.bs))
-		self.surface.blit(self.playerblockdark,(self.pos[0]*self.bs+1, self.pos[1]*self.bs+1))
+        # update positions
+        if move == 1:  # UP
+            pos = [pos[0], pos[1] - 1]
+        elif move == 2:  # DOWN
+            pos = [pos[0], pos[1] + 1]
+        elif move == 3:  # LEFT
+            pos = [pos[0] - 1, pos[1]]
+        elif move == 4:  # RIGHT
+            pos = [pos[0] + 1, pos[1]]
 
+        self.pos = pos
+
+    def draw(self):
+        self.surface.blit(self.playerblock,
+                          (self.pos[0] * self.bs, self.pos[1] * self.bs))
+        self.surface.blit(self.playerblockdark,
+                          (self.pos[0] * self.bs + 1,
+                           self.pos[1] * self.bs + 1))
